20228f89a987fd3dc2df2712e620a95b1690fdaa
Mehedi86 <abdullahalmehedi@gmail.com>
2025-03-28 15:37:10 +0600

    added: all book api

--- index.js
+++ index.js
@@ -30,9 +30,23 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // books related collection
+        const booksCollection = client.db('edulab').collection('allBooks');
+
+        // apis
+        app.get('/books', async (req, res) => {
+            const cursor = booksCollection.find();
+            const result = await cursor.toArray() ;
+            res.send(result)
+        })
+
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
