620f745563508ee13b4273a8656e21a9a2cfa169
Mehedi86 <abdullahalmehedi@gmail.com>
2025-03-30 05:58:08 +0600

    added: return functionality

--- index.js
+++ index.js
@@ -97,6 +97,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/myBorrowedBook/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await borrowCollection.deleteOne(query);
+            res.send(result)
+        })
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
